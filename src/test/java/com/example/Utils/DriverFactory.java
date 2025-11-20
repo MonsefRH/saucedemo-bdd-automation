fc940a18176cd09a487f59c054074639f411bc3f
MonsefRH <monsef.rh15@gmail.com>
2025-11-20 02:07:46 +0100

    add chrome configurations for github actions

--- src/test/java/com/example/Utils/DriverFactory.java
+++ src/test/java/com/example/Utils/DriverFactory.java
@@ -12,7 +12,14 @@ public class DriverFactory {
         if (driver.get() == null) {
             WebDriverManager.chromedriver().setup();
             ChromeOptions options = new ChromeOptions();
-            options.addArguments("--start-maximized");
+            // MODE HEADLESS OBLIGATOIRE SUR GITHUB ACTIONS
+            options.addArguments("--headless=new");           // Chrome 109+
+            options.addArguments("--no-sandbox");
+            options.addArguments("--disable-dev-shm-usage");
+            options.addArguments("--disable-gpu");
+            options.addArguments("--window-size=1920,1080");
+            options.addArguments("--disable-extensions");
+            options.addArguments("--remote-debugging-port=9222");
             driver.set(new ChromeDriver(options));
         }
         return driver.get();
